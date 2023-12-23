ded5d217969eaa1f3be44522611dc388806c0ef8
Daniel Zaiser <danielzaiser91@googlemail.com>
2023-12-23 02:38:57 +0100

    fix yt no interest

--- utility.js
+++ utility.js
@@ -1549,7 +1549,7 @@ function noInterestButton() {
         document.body.classList.add('cu-menu--hide');
         dropdownTrigger.click();
         setTimeout(() => {
-          const path = query('ytd-menu-popup-renderer [d="M18.71 6C20.13 7.59 21 9.69 21 12c0 4.97-4.03 9-9 9-2.31 0-4.41-.87-6-2.29L18.71 6zM3 12c0-4.97 4.03-9 9-9 2.31 0 4.41.87 6 2.29L5.29 18C3.87 16.41 3 14.31 3 12zm9-10c5.52 0 10 4.48 10 10s-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2z"]');
+          const path = query('ytd-menu-popup-renderer ytd-menu-service-item-renderer:nth-last-child(3)');
           if (!path) return;
           const noInterestBtn = path.closest('ytd-menu-service-item-renderer');
           if (noInterestBtn) noInterestBtn.click();

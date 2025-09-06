2151c683150ae620210c1d44e9263b945d83b249
Daniel Zaiser <danielzaiser91@googlemail.com>
2025-09-07 00:09:28 +0200

    1.042d - fix google maps ui toggle not showing for user generated content...

--- utility.js
+++ utility.js
@@ -1454,13 +1454,8 @@ function fixGoogleMaps() {
       el.style.left = right + 10 + 'px';
       if (window._cu_check_search_el_changed) clearInterval(window._cu_check_search_el_changed);
       window._cu_check_search_el_changed = setInterval(() => {
-        const notOnStreetView = !location.href.includes('streetview');
-        if (notOnStreetView || !searchBox || !searchBox.checkVisibility() || parseFloat(getComputedStyle(searchBox).width) === 0) el?.classList.add('cu-hide');
+        if (!searchBox || !searchBox.checkVisibility() || parseFloat(getComputedStyle(searchBox).width) === 0) el?.classList.add('cu-hide');
         else el?.classList.remove('cu-hide');
-        if (notOnStreetView) {
-          visible = false;
-          return toggleVisibility(!visible);
-        }
         const { right } = searchBox.getBoundingClientRect();
         el.style.left = right + 10 + 'px';
       }, 500);

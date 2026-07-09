c99271d740846aa22c9ad91ca085f6eb60cbc021
Daniel Zaiser <danielzaiser91@googlemail.com>
2026-07-09 23:07:55 +0200

    1.3.8 - version bump

--- utility.js
+++ utility.js
@@ -5184,7 +5184,7 @@ let ascending = false;
 let sortButton;
 let userOptions = {
   // key must be match.site lowercased (saved as matcher globally)
-  version: "1.3.7",
+  version: "1.3.8",
   ds3cheatsheet: {
     featureDarkMode: {
       featureName: "DarkMode",

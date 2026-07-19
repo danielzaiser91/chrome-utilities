ba373ac3c86fef0f5934044fb685aaa20839045d
Daniel Zaiser <danielzaiser91@googlemail.com>
2026-07-19 19:14:56 +0200

    1.4.3 - version bump
    Fixes Crunchyroll autoskip undoing the native "resume where you left
    off" seek by detecting the resume jump instead of guessing a fixed
    delay.

--- utility.js
+++ utility.js
@@ -5117,7 +5117,7 @@ let ascending = false;
 let sortButton;
 let userOptions = {
   // key must be match.site lowercased (saved as matcher globally)
-  version: "1.4.2",
+  version: "1.4.3",
   ds3cheatsheet: {
     featureDarkMode: {
       featureName: "DarkMode",
